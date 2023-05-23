60d460fd4348b42cd0929d199af30f8fb009809d
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2023-05-23 11:56:20 +0300

    created html/css

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function switchToRegister() {
+    document.getElementById('login-col').style.display = 'none';
+    document.getElementById('register-col').style.display = 'block';
+}
+
+
+function switchToLogin() {
+    document.getElementById('register-col').style.display = 'none';
+    document.getElementById('login-col').style.display = 'block';
+    }
